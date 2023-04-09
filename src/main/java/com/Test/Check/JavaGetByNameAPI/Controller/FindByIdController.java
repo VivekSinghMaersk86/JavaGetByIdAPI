fe6d0f53d6fb8e10a3d0d0937ccf15bb60cfbda5
vivek <viveksinghjava87@gmail.com>
2023-04-09 13:45:02 +0530

    Java Get API -Individual

--- src/main/java/com/Test/Check/JavaGetByNameAPI/Controller/FindByIdController.java
+++ src/main/java/com/Test/Check/JavaGetByNameAPI/Controller/FindByIdController.java
@@ -6,8 +6,6 @@ import com.Test.Check.JavaGetByNameAPI.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class FindByIdController {
 
